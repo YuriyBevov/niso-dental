c8c03a566669618f8bb93b10bc8d9050c3c53e43
Yuriy Bevov <yuriybevov@gmail.com>
2025-06-19 11:35:48 +0300

    правка маски для полей с телефоном

--- local/frontend/src/js/modules/input-number-mask.js
+++ local/frontend/src/js/modules/input-number-mask.js
@@ -1,9 +1,9 @@
 import IMask from "imask";
 
-const phoneFields = document.querySelectorAll('[type="tel"]');
+const phoneFields = document.querySelectorAll('[data-type="tel"]');
 
 const maskOptions = {
-	mask: "+{7}(000) 000 - 00 - 00",
+	mask: "+{7}(000) 000-00-00",
 };
 
 phoneFields.forEach((field) => {
