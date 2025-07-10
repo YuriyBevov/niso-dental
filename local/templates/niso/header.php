2cc651723a2c1eb64628ce13129390146782d510
Yuriy Bevov <yuriybevov@gmail.com>
2025-07-10 10:51:51 +0300

    Установил метрику

--- local/templates/niso/header.php
+++ local/templates/niso/header.php
@@ -28,11 +28,39 @@ $curPage = $APPLICATION->GetCurPage();
   // Для подключения скриптов
   $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . "/assets/build.js");
   $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . "/assets/custom.js");
+
   ?>
   <meta name="yandex-verification" content="de1ec18da1a2ca85" />
+
+  <script type="text/javascript">
+    (function(m, e, t, r, i, k, a) {
+      m[i] = m[i] || function() {
+        (m[i].a = m[i].a || []).push(arguments)
+      };
+      m[i].l = 1 * new Date();
+      for (var j = 0; j < document.scripts.length; j++) {
+        if (document.scripts[j].src === r) {
+          return;
+        }
+      }
+      k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+    })
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(98000599, "init", {
+      clickmap: true,
+      trackLinks: true,
+      accurateTrackBounce: true,
+      webvisor: true
+    });
+  </script>
+
 </head>
 
 <body>
+  <noscript>
+    <div><img src="https://mc.yandex.ru/watch/98000599" style="position:absolute; left:-9999px;" alt="" /></div>
+  </noscript>
   <div id="panel"><? $APPLICATION->ShowPanel(); ?></div>
 
   <header class="header">
