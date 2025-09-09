0db8f127a29fe771a9c95d6672a6f5dbaaa66769
Yuriy Bevov <yuriybevov@gmail.com>
2025-09-09 09:33:45 +0300

    Установил фавикон

--- local/templates/niso/header.php
+++ local/templates/niso/header.php
@@ -12,6 +12,11 @@ $curPage = $APPLICATION->GetCurPage();
   <? $APPLICATION->ShowHead(); ?>
   <title><? $APPLICATION->ShowTitle() ?></title>
 
+  <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+  <link rel="shortcut icon" href="/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <link rel="manifest" href="/site.webmanifest" />
 
   <?
   // Подключение мета тегов или сторонних файлов
