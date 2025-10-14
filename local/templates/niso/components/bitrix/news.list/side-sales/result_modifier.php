162f1a6c064464ddaa67baf4798402c6e59d5930
Oksana Tarasova <oksanchik_90@mail.ru>
2025-10-14 16:26:02 +0300

    Вывели пример на страницу услуги

--- local/templates/niso/components/bitrix/news.list/side-sales/result_modifier.php
+++ local/templates/niso/components/bitrix/news.list/side-sales/result_modifier.php
@@ -1,5 +1,8 @@
 <? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 
-$arResult["ITEMS"] = array_filter($arResult["ITEMS"], function ($arItem) {
-  return ($arItem["PROPERTIES"]["SHOW_IN_HEAD_SECTION"]["VALUE"] ?? '') === 'Y';
-});
+if(($arParams["CUSTOM_IS_HEAD_SALE_BLOCK"] ?? '') === 'Y') {
+  $arResult["ITEMS"] = array_filter($arResult["ITEMS"], function ($arItem) {
+    return ($arItem["PROPERTIES"]["SHOW_IN_HEAD_SECTION"]["VALUE"] ?? '') === 'Y';
+  });
+}
+
