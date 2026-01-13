dc746d2279663d144c064d867ebbb40e12b1fc47
Oksana Tarasova <oksanchik_90@mail.ru>
2026-01-13 16:50:30 +0300

    Исправление компонента с ценами, чтобы элементы можно было редактировать на странице

--- local/templates/niso/components/bitrix/news.list/prices/template.php
+++ local/templates/niso/components/bitrix/news.list/prices/template.php
@@ -1,4 +1,4 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 $this->setFrameMode(true);
 ?>
 
@@ -8,23 +8,20 @@ $this->setFrameMode(true);
 			<span class="base-text base-section__headline">
 			</span>
 			<h1 class="base-title"><?= $arResult["NAME"] ?></h1>
-			<?if (!empty($arResult['DESCRIPTION'])) :?>
-				<?if ($arResult['DESCRIPTION_TYPE'] == 'html') :?>
-					<div class="content"><?= $arResult["DESCRIPTION"]?></div>
-				<?else :?>
-					<span class="base-text"><?= $arResult["DESCRIPTION"]?></span>
-				<?endif;?>
-			<?endif;?>
+			<? if (!empty($arResult['DESCRIPTION'])) : ?>
+				<? if ($arResult['DESCRIPTION_TYPE'] == 'html') : ?>
+					<div class="content"><?= $arResult["DESCRIPTION"] ?></div>
+				<? else : ?>
+					<span class="base-text"><?= $arResult["DESCRIPTION"] ?></span>
+				<? endif; ?>
+			<? endif; ?>
 		</div>
 		<div class="accordeon --first-item-expanded">
-			<? foreach ($arResult['SECTIONS'] as $arSection):
-				$this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], CIBlock::GetArrayByID($arSection["IBLOCK_ID"], "ELEMENT_EDIT"));
-				$this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], CIBlock::GetArrayByID($arSection["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-			?>
-				<div class="accordeon-item" id="<?= $this->GetEditAreaId($arSection['ID']); ?>">
+			<? foreach ($arResult['SECTIONS'] as $arSection): ?>
+				<div class="accordeon-item">
 					<div class="accordeon-header">
 						<span class="base-subtitle">
-						<?= $arSection["NAME"] ?></span>
+							<?= $arSection["NAME"] ?></span>
 						<div class="accordeon-opener">
 							<svg width="40" height="40" role="img" aria-hidden="true" focusable="false">
 								<use xlink:href="<?= SITE_TEMPLATE_PATH ?>/assets/sprite.svg#icon-arrow-down"></use>
@@ -33,15 +30,19 @@ $this->setFrameMode(true);
 					</div>
 					<div class="accordeon-body">
 						<div class="content-block">
-								<? foreach ($arSection['ITEMS'] as $arItem): ?>
-									<div class="content-block__item">
-										<span><?=$arItem['NAME']?></span>
-										<div class="content-block__wrapper">
-											<strong><?=$arItem['PROPERTIES']['SERVICE_PRICE']['VALUE']?></strong>
-											<button class="main-btn" data-modal-opener="callback-modal" data-service-name="<?=$arItem['NAME'], $arItem['PROPERTIES']['SERVICE_PRICE']['VALUE']?>">Записаться на прием</button>
-										</div>
+							<? foreach ($arSection['ITEMS'] as $arItem): ?>
+								<?
+								$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+								$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+								?>
+								<div class="content-block__item" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
+									<span><?= $arItem['NAME'] ?></span>
+									<div class="content-block__wrapper">
+										<strong><?= $arItem['PROPERTIES']['SERVICE_PRICE']['VALUE'] ?></strong>
+										<button class="main-btn" data-modal-opener="callback-modal" data-service-name="<?= $arItem['NAME'], $arItem['PROPERTIES']['SERVICE_PRICE']['VALUE'] ?>">Записаться на прием</button>
 									</div>
-								<?endforeach; ?>
+								</div>
+							<? endforeach; ?>
 							</ul>
 						</div>
 					</div>
