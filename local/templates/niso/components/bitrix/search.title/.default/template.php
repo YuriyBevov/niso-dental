b093dee1e124600b93709a7541377a206d019559
OkTarasova <118619788+OkTarasova@users.noreply.github.com>
2026-08-19 15:58:44 +0300

    Правлю стили в поиске

--- local/templates/niso/components/bitrix/search.title/.default/template.php
+++ local/templates/niso/components/bitrix/search.title/.default/template.php
@@ -1,6 +1,5 @@
 <?php
-if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
-{
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 	die();
 }
 /** @var array $arParams */
@@ -14,38 +13,37 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 /** @var string $templateFolder */
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
-$this->setFrameMode(true);?>
+$this->setFrameMode(true); ?>
+
 <?php
 $INPUT_ID = trim($arParams['~INPUT_ID']);
-if ($INPUT_ID == '')
-{
+if ($INPUT_ID == '') {
 	$INPUT_ID = 'title-search-input';
 }
 $INPUT_ID = CUtil::JSEscape($INPUT_ID);
 
 $CONTAINER_ID = trim($arParams['~CONTAINER_ID']);
-if ($CONTAINER_ID == '')
-{
+if ($CONTAINER_ID == '') {
 	$CONTAINER_ID = 'title-search';
 }
 $CONTAINER_ID = CUtil::JSEscape($CONTAINER_ID);
 
-if ($arParams['SHOW_INPUT'] !== 'N'):?>
-	<div id="<?php echo $CONTAINER_ID?>">
-		<form class="search-form" action="<?php echo $arResult['FORM_ACTION']?>">
-			<input class="search-form__input" id="<?php echo $INPUT_ID?>" type="text" name="q" value="" size="40" maxlength="50" autocomplete="off" placeholder="Поиск" />
+if ($arParams['SHOW_INPUT'] !== 'N'): ?>
+	<div id="<?php echo $CONTAINER_ID ?>">
+		<form class="search-form" action="<?php echo $arResult['FORM_ACTION'] ?>">
+			<input class="search-form__input" id="<?php echo $INPUT_ID ?>" type="text" name="q" value="" size="40" maxlength="50" autocomplete="off" placeholder="Поиск" />
 			<button class="search-form__submit-btn" type="submit">
 				<img src="/img/search.svg" alt="Искать на странице" width="24" height="24">
 			</button>
 		</form>
 	</div>
-<?php endif?>
+<?php endif ?>
 <script>
-	BX.ready(function(){
+	BX.ready(function() {
 		new JCTitleSearch({
-			'AJAX_PAGE' : '<?php echo CUtil::JSEscape(POST_FORM_ACTION_URI)?>',
-			'CONTAINER_ID': '<?php echo $CONTAINER_ID?>',
-			'INPUT_ID': '<?php echo $INPUT_ID?>',
+			'AJAX_PAGE': '<?php echo CUtil::JSEscape(POST_FORM_ACTION_URI) ?>',
+			'CONTAINER_ID': '<?php echo $CONTAINER_ID ?>',
+			'INPUT_ID': '<?php echo $INPUT_ID ?>',
 			'MIN_QUERY_LEN': 2
 		});
 	});
