c809833d54f388a37b76d8f35fbdd32ae9a53f8d
OkTarasova <118619788+OkTarasova@users.noreply.github.com>
2026-08-21 14:38:28 +0300

    Меняю код в бококвом меню на старнице услуг по замечанию валидатора

--- local/templates/niso/components/bitrix/menu/side-menu/result_modifier.php
+++ local/templates/niso/components/bitrix/menu/side-menu/result_modifier.php
@@ -1,7 +1,15 @@
 <? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 // Удаляю первый уровень меню(сам раздел) все пустые внутрение разделы
-foreach ($arResult as $key => $arItem) {
-  if ($arItem["DEPTH_LEVEL"] == 1 || $arItem["DEPTH_LEVEL"] == 2 && !$arItem["IS_PARENT"]) {
+foreach ($arResult as $key => &$arItem) {
+  if (
+    $arItem["DEPTH_LEVEL"] == 1 ||
+    ($arItem["DEPTH_LEVEL"] == 2 && !$arItem["IS_PARENT"])
+  ) {
     unset($arResult[$key]);
+    continue;
   }
+
+  $arItem["DEPTH_LEVEL"]--;
 }
+
+unset($arItem);
